c48945c094129726326a9bdaf5c1bc389a2a0e61
zlatsic <zlatan.sicanica@gmail.com>
2022-06-06 18:11:32 +0200

    deploy

---
+++ assets/js/27.452f9656.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{473:function(t,e,n){"use strict";n.r(e);var o=n(66),s=Object(o.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("portfolio")],1)}),[],!1,null,null,null);e.default=s.exports}}]);
