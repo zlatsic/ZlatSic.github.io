4ceb9a309086454d1701c4a1a56b99d798daa6c2
zlatsic <zlatan.sicanica@gmail.com>
2022-08-29 16:58:33 +0200

    cookies - bug correction

--- docs/.vuepress/components/Cookies.vue
+++ docs/.vuepress/components/Cookies.vue
@@ -1,28 +1,35 @@
 <template>
-<div class="cookies-dialogue" v-if="!cookiesDismissed">
-    <span>This website uses cookies to analyze user traffic.</span>
-    <button v-on:click="dismiss">Dismiss</button>
+<div class="wrapper">
+    <div class="cookies-dialogue" v-if="!cookiesDismissed">
+        <span>This website uses cookies to analyze user traffic. {{ cookiesDismissed }}</span>
+        <button v-on:click="dismiss">Dismiss</button>
+    </div>
 </div>
 </template>
 <script>
 export default {
     data: function () {
 	let cookiesDismissed = false;
-	try {
-	    cookiesDismissed = sessionStorage.getItem('cookiesDismissed')
-	} catch (e) {
-	    console.warn('Failed to access sessionStorage', e)
-	}
-	return { cookiesDismissed: cookiesDismissed }
+	return { cookiesDismissed: cookiesDismissed };
+    },
+    mounted: function() {
+        if (typeof window !== 'undefined')
+            try {
+                this.cookiesDismissed = JSON.parse(
+                    localStorage.getItem('cookiesDismissed'));
+            } catch (e) {
+                console.warn('Failed to access localStorage', e);
+            }
     },
     methods: {
         dismiss: function(ev) {
-	    this.cookiesDismissed = true;
-	    try {
-		sessionStorage.setItem('cookiesDismissed', true)
-	    } catch (e) {
-		console.warn('Failed to access sessionStorage', e)
-	    }
+            this.cookiesDismissed = true;
+            if (typeof window !== 'undefined')
+                try {
+                    localStorage.setItem('cookiesDismissed', true);
+                } catch (e) {
+                    console.warn('Failed to access localStorage', e);
+                }
         }
     }
 };
@@ -31,31 +38,34 @@ export default {
 
 $color-maroon: #660018;
 
-.cookies-dialogue {
-    position: fixed;
-    bottom: 1rem;
-    left: 50%;
-    transform: translate(-50%, 0%);
-    z-index: 1;
-    background-color: white;
-    width: fit-content;
-    border-radius: 5px;
-    border: 1.5px solid #660018;
-    padding: 0.5rem;
+.wrapper {
 
-    & > button {
-        margin-left: 1rem;
+    & > .cookies-dialogue {
+        position: fixed;
+        bottom: 1rem;
+        left: 50%;
+        transform: translate(-50%, 0%);
+        z-index: 1;
+        background-color: white;
+        width: fit-content;
+        border-radius: 5px;
+        border: 1.5px solid #660018;
+        padding: 0.5rem;
 
-        background-color: $color-maroon;
-        color: #fff;
-        padding: 0.1rem 0.4rem;
-        border-radius: 4px;
-        transition: background-color 0.1s ease;
-        font-weight: 500;
+        & > button {
+            margin-left: 1rem;
 
-        &:hover {
-            background-color: lighten($color-maroon, 5%);
-            cursor: pointer;
+            background-color: $color-maroon;
+            color: #fff;
+            padding: 0.1rem 0.4rem;
+            border-radius: 4px;
+            transition: background-color 0.1s ease;
+            font-weight: 500;
+
+            &:hover {
+                background-color: lighten($color-maroon, 5%);
+                cursor: pointer;
+            }
         }
     }
 }
